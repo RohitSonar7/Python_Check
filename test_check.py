0fe9990cef20edf3e380bd76f86adf3e1af504ec
unknown <sonarrohit7@gmail.com>
2022-03-28 17:09:14 +0530

    Commit

--- test_check.py
+++ test_check.py
@@ -1,43 +1,48 @@
 import Check
 import pytest
 
-def test_By5_1():
-    x = 10
-    result = Check.By5(x)
+@pytest.fixture()
+def input():
+    a = 25
+    return a
+
+def test_By5_1(input):
+
+    result = Check.By5(input)
     assert result == True
 
-def test_By5_2():
-    x = 12
-    result = Check.By5(x)
+def test_By5_2(input):
+
+    result = Check.By5(input)
     assert result == False
 
-def test_By7_1():
-    x = 14
-    result = Check.By7(x)
+def test_By7_1(input):
+
+    result = Check.By7(input)
     assert result == True
 
-def test_By7_2():
-    x = 11
-    result = Check.By7(x)
+def test_By7_2(input):
+
+    result = Check.By7(input)
     assert result == False
-def test_By9_1():
-    x = 18
-    result = Check.By9(x)
+def test_By9_1(input):
+
+    result = Check.By9(input)
     assert result == True
 
-def test_By9_2():
-    x = 10
-    result = Check.By9(x)
+def test_By9_2(input):
+
+    result = Check.By9(input)
     assert result == False
 
-def test_By11_1():
-    x = 22
-    result = Check.By11(x)
+def test_By11_1(input):
+
+    result = Check.By11(input)
     assert result == True
 
-def test_By11_2():
-    x = 10
-    result = Check.By11(x)
+def test_By11_2(input):
+
+    result = Check.By11(input)
     assert result == False
 
 
